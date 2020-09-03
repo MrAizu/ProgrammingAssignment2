2fa06f7723042dd4691c02f94b8bec67860582af
MrAizu <mrmaizu95@gmail.com>
2020-09-03 13:33:45 +0800

    The latest edit

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,21 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## By default x is a vector object of a matrix class
+## makeCacheMatrix is just a list of user defined-function
+## Assign m object as a inverse function 
 makeCacheMatrix <- function(x = matrix()) {
-
+            m       <- NULL
+            setxobj <- function(a){
+              x <<- a
+              m <<- NULL
+            }
+        setinvobjx <- function(invtmp) m <<- invtmp
+        getinvobjx <- function() m
+        getobjx     <- function() x 
+##list all the function in a vector
+list(set = setxobj, get = getobjx, setinv = setinvobjx , getinv = getinvobjx)
+        
 }
 
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if (!is.null(m)) {
+          message("getting cached data")
+          return(m)
+        }
+        mat_tmp <- x$get()
+        mat_inv <- solve(mat_tmp, ...)
+        x$setinv(mat_inv)
+        m 
+        
 }
